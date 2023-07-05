1dcf36d86aa37e3b0d42d0b5b3159d77b7638858
Damon Pickett <damonpickett@Damons-Mac-mini.local>
2023-07-05 13:56:11 -0700

    completed Home component for all screen sizes

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,23 +1,47 @@
-import './Home.css';
-import profilePic from '../../assets/profile-pic.jpeg'
-
+import "./Home.css";
+import profilePic from "../../assets/profile-pic.jpeg";
+import ReactLogo from "../../assets/Desktop/Tablet/react.png";
+import PythonLogo from "../../assets/Desktop/Tablet/python.png";
+import AILogo from "../../assets/Desktop/Tablet/ai.png";
+import FigmaLogo from "../../assets/Desktop/Tablet/figma.png";
 
 const Home = () => {
   return (
-    <div className='home'>
-        <div className='profile-container'>
-            <img src={profilePic} className='profile-pic' alt='Damon Pickett' />
+    <div className="home">
+      <div className="hero">
+        <div className="profile-container">
+          <img src={profilePic} className="profile-pic" alt="Damon Pickett" />
         </div>
-        <div className='site-title'>
+        <div className="hero-text">
+          <div className="site-title">
             <h1>Damon the Dev</h1>
-        </div>
-        <div className='site-subtitle'>
+          </div>
+          <div className="site-subtitle">
             <h2>Software Developer</h2>
+          </div>
+          <div className="value-prop">
+            <p>
+              Unlock the power of seamless user experiences and intelligent
+              solutions with a front-end and AI specialist.
+            </p>
+          </div>
+          <button className="contact">Get in touch</button>
         </div>
-        <div className='value-prop'>
-            <p>Unlock the power of seamless user experiences and intelligent solutions with a front-end and AI specialist.</p>
+      </div>
+      <div className="tech-stack">
+        <div className="tech-item">
+          <img src={ReactLogo} alt="react" />
         </div>
-        <button className='contact'>Get in touch</button>
+        <div className="tech-item">
+          <img src={PythonLogo} alt="python" />
+        </div>
+        <div className="tech-item">
+          <img src={AILogo} alt="ai" />
+        </div>
+        <div className="tech-item">
+          <img src={FigmaLogo} alt="figma" />
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/MainContent/MainContent.js
+++ src/components/MainContent/MainContent.js
@@ -1,12 +1,12 @@
 import "./MainContent.css";
-import github from '../../assets/github.png';
-import twitter from '../../assets/twitter.png';
-import linkedin from '../../assets/linkedin.png';
-import { Routes, Route } from 'react-router-dom';
-import Home from '../Home/Home';
-import Projects from '../Projects/Projects';
-import Experience from '../Experience/Experience';
-import About from '../About/About';
+import github from "../../assets/github.png";
+import twitter from "../../assets/twitter.png";
+import linkedin from "../../assets/linkedin.png";
+import { Routes, Route } from "react-router-dom";
+import Home from "../Home/Home";
+import Projects from "../Projects/Projects";
+import Experience from "../Experience/Experience";
+import About from "../About/About";
 
 function MainContent() {
   return (
@@ -18,29 +18,51 @@ function MainContent() {
         <Route path="/projects" element={<Projects />} />
         <Route path="/experience" element={<Experience />} />
       </Routes>
-      <div className="credits">
-        <p>
-          Designed and{" "}
+      <div className="footer-content">
+        <div className="credits">
+          <p>
+            Designed and{" "}
+            <a
+              href="https://github.com/damonpickett/damon-the-dev"
+              rel="noreferrer"
+              target="_blank"
+            >
+              built
+            </a>{" "}
+            by Damon the Dev
+          </p>
+          <p>
+            Icons by{" "}
+            <a href="https://icons8.com/" rel="noreferrer" target="_blank">
+              Icons8
+            </a>
+          </p>
+        </div>
+        <div className="social">
           <a
-            href="https://github.com/damonpickett/damon-the-dev"
+            href="https://twitter.com/Damon_Pickett"
+            rel="noreferrer"
+            target="_blank"
+            className="first"
+          >
+            <img src={twitter} alt="twitter" />
+          </a>
+          <a
+            href="https://github.com/damonpickett"
+            rel="noreferrer"
+            target="_blank"
+            className="second"
+          >
+            <img src={github} alt="github" />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/damon-pickett/"
             rel="noreferrer"
             target="_blank"
           >
-            built
-          </a>{" "}
-          by Damon the Dev
-        </p>
-        <p>
-          Icons by{" "}
-          <a href="https://icons8.com/" rel="noreferrer" target="_blank">
-            Icons8
+            <img src={linkedin} alt="linkedin" />
           </a>
-        </p>
-      </div>
-      <div className="social">
-        <a href='https://twitter.com/Damon_Pickett' rel='noreferrer' target='_blank'><img src={twitter} alt='twitter' /></a>
-        <a href='https://github.com/damonpickett' rel='noreferrer' target='_blank'><img src={github} alt='twitter' /></a>
-        <a href='https://www.linkedin.com/in/damon-pickett/' rel='noreferrer' target='_blank'><img src={linkedin} alt='twitter' /></a>
+        </div>
       </div>
     </div>
   );
